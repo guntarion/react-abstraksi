a0bd2ab3538ac1980a6c852167373cd83bc4a740
Akhmad Guntar <guntar@me.com>
2024-08-08 21:59:17 +0700

    first commit

---
+++ build/precache-manifest.f573581c22ccac81b37050669f257bf1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "98810181efbb3dd37a85",
+    "url": "/static/css/main.1c2caab6.chunk.css"
+  },
+  {
+    "revision": "98810181efbb3dd37a85",
+    "url": "/static/js/main.74a60707.chunk.js"
+  },
+  {
+    "revision": "5ed7f9698f028a55913f",
+    "url": "/static/js/runtime~main.3c9219f4.js"
+  },
+  {
+    "revision": "5ac245ac5379e1ef9991",
+    "url": "/static/css/2.5b080c65.chunk.css"
+  },
+  {
+    "revision": "5ac245ac5379e1ef9991",
+    "url": "/static/js/2.ff22a073.chunk.js"
+  },
+  {
+    "revision": "a46865ad8acf0259945f",
+    "url": "/static/js/3.5356498d.chunk.js"
+  },
+  {
+    "revision": "d0173d54211058cd7ffca28f8f145b23",
+    "url": "/index.html"
+  }
+];
